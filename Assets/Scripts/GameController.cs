df0f8287d3bc1a0401a35885f96fa0526fd874ff
Vincent Deloso <Mitsugaru@users.noreply.github.com>
2017-12-29 19:01:12 -0700

    Added a Lane layer. Updated the NewGame method with more functionality. Minor formatting.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,7 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
+
+    public CardManager cardManager;
+
+    public ArenaManager arenaManager;
+
+    public DeckScript visualPlayerDeck;
+
+    public DeckScript visualOpponentDeck;
+
+    public HandManager playerHand;
+
+    public HandManager opponentHand;
 
     private PlayerDeck playerDeck;
 
@@ -12,24 +25,33 @@ public class GameController : MonoBehaviour {
 
     private bool addJokers = true;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         playerDeck = new PlayerDeck();
         opponentDeck = new PlayerDeck();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public void NewGame()
     {
         //TODO detect if there is a current game already and ask if player wants to abandon current
-        
+
+        // Clear the arena
+        arenaManager.Clear();
+        playerHand.Clear();
+        opponentHand.Clear();
+        visualPlayerDeck.SpawnDeck();
+        visualOpponentDeck.SpawnDeck();
+
         //TODO check if this is a rematch, if so, auto switch colors
 
-        if(Random.value > 0.5f)
+        if (Random.value > 0.5f)
         {
             playerFirst = true;
         }
@@ -55,19 +77,58 @@ public class GameController : MonoBehaviour {
         }
 
         playerDeck.AddRange(playerCards);
+        playerDeck.ShuffleAll();
         opponentDeck.AddRange(opponentCards);
+        opponentDeck.ShuffleAll();
+
+        // Draw initial hands as a fast coroutine for animation to occur
+        StartCoroutine(initialHandDraw());
 
         //TODO Start setup portion of game
+
         //TODO once setup is complete, start game loop
+    }
+
+    private IEnumerator initialHandDraw()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Card playerCard = playerDeck.Draw();
+            GameObject playerCardGameObject = cardManager.SpawnCard(playerCard);
+            playerHand.AddCard(playerCardGameObject);
+            visualPlayerDeck.DrawCard();
+            yield return new WaitForSeconds(0.3f);
+
+            Card opponentCard = opponentDeck.Draw();
+            GameObject opponentCardGameObject = cardManager.SpawnCard(opponentCard);
+            opponentHand.AddCard(opponentCardGameObject);
+            visualOpponentDeck.DrawCard();
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+    public void playerDraw()
+    {
+        Card card = playerDeck.Draw();
+        if (card != null)
+        {
+            visualPlayerDeck.DrawCard();
+        }
+        else
+        {
+            //TODO need to check if the player hands still have cards to play
+            // otherwise, we need to reveal and resolve the remaining cards
+            // on the table
+        }
     }
 
     private IList<Card> generateCardsForSuit(Suit suit)
     {
         IList<Card> cards = new List<Card>();
 
-        foreach(Rank rank in Rank.Values)
+        foreach (Rank rank in Rank.Values)
         {
-            if(rank != Rank.JOKER)
+            if (rank != Rank.JOKER)
             {
                 cards.Add(new Card(rank, suit));
             }
