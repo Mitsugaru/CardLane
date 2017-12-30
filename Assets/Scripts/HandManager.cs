3bab4aacb82d215580594e1d188efe2234870c6f
Vincent Deloso <Mitsugaru@users.noreply.github.com>
2017-12-29 22:25:20 -0700

    Updated hand manager to handle the positioning of cards in hand / selection. Added highlight support to the hand manager. Added a flag to disable input, used for the opponent hand which shouldn't be selectable by the player. Added a card comparer for sorting game objects with a CardScript component.

--- Assets/Scripts/HandManager.cs
+++ Assets/Scripts/HandManager.cs
@@ -8,6 +8,13 @@ public class HandManager : MonoBehaviour
     //TODO inject later
     public CardManager cardManager;
 
+    public HighlightManager highlightManager;
+
+    public bool enableInput = true;
+
+    [Range(-0.1f, -0.5f)]
+    public float selectionDistance = -0.3f;
+
     private Quaternion handAngle;
 
     private float scalingFactor = 10f;
@@ -20,8 +27,7 @@ public class HandManager : MonoBehaviour
 
     private GameObject selectedCard;
 
-    //TODO manage the rotation of cards based on number in hand
-    //TODO lerp / animate
+    private readonly IComparer<GameObject> cardCompare = new CardComparer();
 
     // Use this for initialization
     void Start()
@@ -33,26 +39,76 @@ public class HandManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchSupported)
+        if (enableInput && Input.touchSupported)
         {
             handleTouch();
         }
-        else
+        else if(enableInput)
         {
             handleMouseAndKeyboard();
         }
 
-        //TODO manage card movement
+        // manage card movement
         if (cards.Count > 0)
         {
             if (moveLapse < 1f)
             {
-                //TODO foreach card, move them according to size of current hand
+                // foreach card, move them according to size of current hand
+                Vector3 targetPosition = new Vector3(0f, 0f, 0f);
+                bool flip = false;
+                int magnitude = 1;
+                float distance = 0.8f;
+                //TODO it'd be cool if the order of the cards was correct based on positioning
+                for(int i = 0; i < cards.Count; i++)
+                {
+                    Vector3 currentPosition = cards[i].transform.localPosition;
+
+                    //Also take into account selection
+                    if (cards[i].Equals(selectedCard))
+                    {
+                        targetPosition.x = selectionDistance;
+                    }
+                    else
+                    {
+                        targetPosition.x = 0f;
+                    }
+
+                    cards[i].transform.localPosition = Vector3.Lerp(currentPosition, targetPosition, moveLapse);
+
+                    if(flip)
+                    {
+                        targetPosition.z = magnitude * distance;
+                        flip = false;
+                        magnitude++;
+                    }
+                    else
+                    {
+                        targetPosition.z = -magnitude * distance;
+                        flip = true;
+                    }
+                }
+
                 moveLapse += 0.1f;
             }
             if (selectLapse < 1f)
             {
-                //TODO foreach card that isn't selected, ensure that their transform is lerped back to "normal"
+                // foreach card that isn't selected, ensure that their transform is lerped back to "normal"
+                for(int i = 0; i < cards.Count; i++)
+                {
+                    Vector3 currentPosition = cards[i].transform.localPosition;
+                    Vector3 targetPosition = cards[i].transform.localPosition;
+                    if (cards[i].Equals(selectedCard))
+                    {
+                        targetPosition.x = selectionDistance;
+                    }
+                    else
+                    {
+                        targetPosition.x = 0f;
+                    }
+                    cards[i].transform.localPosition = Vector3.Lerp(currentPosition, targetPosition, selectLapse);
+                }
+
+                selectLapse += 0.1f;
             }
         }
     }
@@ -60,6 +116,7 @@ public class HandManager : MonoBehaviour
     public void AddCard(GameObject card)
     {
         moveLapse = 0f;
+        selectLapse = 0f;
 
         card.transform.parent = gameObject.transform;
         card.transform.position = gameObject.transform.position;
@@ -67,18 +124,24 @@ public class HandManager : MonoBehaviour
         card.transform.rotation = handAngle;
         card.layer = LayerMask.NameToLayer("Card");
         card.AddComponent<BoxCollider>();
+
+        cards.Add(card);
+
+        cards.Sort(cardCompare);
     }
 
     public void RemoveCard(GameObject card)
     {
         if (cards.Remove(card))
         {
+            cards.Sort(cardCompare);
             moveLapse = 0f;
         }
     }
 
     public void Clear()
     {
+        highlightManager.select(null);
         moveLapse = 1f;
         selectLapse = 1f;
         cards.Clear();
@@ -88,6 +151,13 @@ public class HandManager : MonoBehaviour
             GameObject.Destroy(child.gameObject);
         }
         transform.DetachChildren();
+    }
+
+    public void ClearSelection()
+    {
+        selectedCard = null;
+        highlightManager.select(null);
+        selectLapse = 0f;
     }
 
     public GameObject GetSelectedCard()
@@ -120,9 +190,9 @@ public class HandManager : MonoBehaviour
 
     private void handleMouseAndKeyboard()
     {
+        GameObject card = detectHandCardHit(Camera.main.ScreenPointToRay(Input.mousePosition));
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject card = detectHandCardHit(Camera.main.ScreenPointToRay(Input.mousePosition));
             if (card != null)
             {
                 selectCard(card);
@@ -131,7 +201,20 @@ public class HandManager : MonoBehaviour
             {
                 //TODO check if a lane was selected
                 //TODO otherwise, deselect hand card
+                selectLapse = 0f;
+                selectedCard = null;
+                highlightManager.select(null);
             }
+        }
+        else if (card != null && selectedCard == null)
+        {
+            // add a hover effect on what would be the current selection
+            highlightManager.select(card.transform);
+        }
+        else if(selectedCard == null)
+        {
+            // remove hover effect
+            highlightManager.select(null);
         }
     }
 
@@ -155,17 +238,12 @@ public class HandManager : MonoBehaviour
 
     private void selectCard(GameObject card)
     {
-        //TODO select the card
-        string cardName = card.name;
         CardScript script = card.GetComponent<CardScript>();
         if (script != null)
         {
-            cardName = script.Card.ToString();
-
             selectedCard = card;
-
-            //TODO selectLapse = 0f;
+            highlightManager.select(card.transform);
+            selectLapse = 0f;
         }
-        Debug.Log("Clicked card: " + cardName);
     }
 }
